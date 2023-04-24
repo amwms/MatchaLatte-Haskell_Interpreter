e190a1ec8d7bbc89cb03267ba6f27d41717037df
Anna Stawiska <ania.stawiska@gmail.com>
2023-04-24 15:20:16 +0200

    Updated execProgComps to have default value when declarin variables and updated evalItems and execProgComp for functions

--- src/Program.hs
+++ src/Program.hs
@@ -30,17 +30,45 @@ execProgram (Program _ components) = do
     env <- ask
     (store, last) <- get
     env' <- local (const env) $ execProgComps components
-    return 0
+    -- TODO -  run all porgram components and then run Expr Application of main
+    -- and return that value
+    -- if hasReturn env' then do
+    --     val <- getValueFromMemory env' store "return"
+    --     case val of
+    --         VInt i -> return $ fromInteger i
+    -- else
+    --     return 0 --TODO ->return main() return 
+    return env'
 
 -- TODO
 execProgComp :: ProgComp -> InterpreterMonad MyEnv
-execProgComp (FunDecl _ typ ident args block) = do
+execProgComp (FunDecl _ retType ident args block) = do
     env <- ask
-    return env
+    (store, loc) <- get
+    let newEnv = Data.Map.insert ident loc env
+    let newStore = (Data.Map.insert (VFun args retType block newEnv) loc store, loc + 1)
+    put newStore
+    return newEnv
 
-execProgComp (VarDecl _ typ items) = do
+execProgComp (VarDecl _ varType items) = do
     env <- ask
-    return env
+    case varType of
+        Int _ -> local (const env) $ evalItems items $ VInt 0
+        Str _ -> local (const env) $ evalItems items $ VStr ""
+        Bool _ -> local (const env) $ evalItems items $ VBool False
+        Void _ -> local (const env) $ evalItems items VVoid
+        Fun _ [Type' _] (Type' _) -> local (const env) $ evalItems items $ VFun [] (Type' _) (Block _ [])
+    
+
+evalItems :: [Item] -> InterpreterMonad MyEnv
+evalItems [] = do
+    ask
+
+evalItems (item : items) = do
+    env <- ask
+    (store, loc) <- get
+    env' <- local (const env) $ evalItem item
+    local (const env') $ evalItems items
 
 evalItem :: Item -> InterpreterMonad MyEnv
 evalItem (NoInit _ ident) defaultValue = do
