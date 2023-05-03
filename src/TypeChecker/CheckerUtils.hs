c77ecb4d1b6c12184c3ba716a2684b5c69a43c66
Anna Stawiska <ania.stawiska@gmail.com>
2023-05-03 22:50:54 +0200

    CheckerUtils added

---
+++ src/TypeChecker/CheckerUtils.hs
@@ -0,0 +1,26 @@
+module CheckerUtils where
+
+import Data.Map
+import Control.Monad.Reader      
+import Control.Monad.Except
+
+import Grammar.Abs
+import CheckerTypes
+
+getVariableType :: Ident -> TypeCheckerMonad Type
+getVariableType pos ident = do
+    env <- ask
+    case Data.Map.lookup ident env of
+        Just identType -> return identType
+        Nothing -> throwError $ show ident ++ " in position " ++ show pos ++ " not found"
+
+hasReturn :: TypeEnv -> Bool
+hasReturn env = case Data.Map.lookup (Ident "return") env of
+    Just _ -> True
+    Nothing -> False
+
+getReturnType :: TypeEnv -> TypeCheckerMonad Type
+getReturnType pos env = 
+    case Data.Map.lookup (Ident "return") env of
+        Just (retType _) -> return (retType pos)
+        Nothing -> return (Void pos)
