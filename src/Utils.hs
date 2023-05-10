ba033a504feb1080074c2acb84017b252ccb1b3d
Anna Stawiska <ania.stawiska@gmail.com>
2023-05-11 00:28:34 +0200

    Added showValue to Utils

--- src/Utils.hs
+++ src/Utils.hs
@@ -3,6 +3,7 @@ module Utils where
 import Grammar.Abs
 import Data.Map
 import Types
+import TypeChecker.CheckerUtils ( showType )
 
 import Control.Monad.Reader      
 import Control.Monad.Except
@@ -12,9 +13,6 @@ getValueFromMemory :: Ident -> InterpreterMonad Value
 getValueFromMemory ident = do
     env <- ask
     (store, _) <- get
-    -- DEBUG
-    -- liftIO (putStrLn $ show ident)
-    -- liftIO (putStrLn $ show env)
     case Data.Map.lookup ident env of
         Just loc -> return (findWithDefault VVoid loc store)
         Nothing -> throwError (show ident ++ " not found")
@@ -36,3 +34,15 @@ getReturnValue env =
     case Data.Map.lookup (Ident "return") env of
         Just loc -> local (const env) $ getValueFromMemory (Ident "return")
         Nothing -> throwError "Return value not found"
+
+showValue :: Value -> String
+showValue (VInt intVal) = show intVal
+showValue (VString str) = str
+showValue (VBool boolVal) = show boolVal
+showValue (VFun args retType block _) = "function" ++ "(" ++ showArgs args ++ ")" ++ " -> " ++ showType retType
+showValue (VVoid) = "void"
+
+showArgs :: [Arg] -> String
+showArgs [] = ""
+showArgs ((ValArg _ argType (Ident name)):args) = showType argType ++ " " ++ name ++ ", " ++ showArgs args
+showArgs ((RefArg _ argType (Ident name)):args) = showType argType ++ " @" ++ name ++ ", " ++ showArgs args
