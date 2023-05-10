9b76662519ef64dd1ef6b3697d3c64b8237d6939
Anna Stawiska <ania.stawiska@gmail.com>
2023-05-11 00:59:16 +0200

    Updated run type-check

--- src/TypeChecker/TypeChecker.hs
+++ src/TypeChecker/TypeChecker.hs
@@ -7,15 +7,7 @@ import Control.Monad.Except
 import Control.Monad.State
 
 import TypeChecker.CheckerTypes
-import TypeChecker.GrammarChecker
-
--- run :: Program -> IO ()
--- run p = 
---     do
---         x <- runExceptT (runReaderT (checkProgram p) empty) 
---         case x of
---             Left err -> print err
---             Right (code, store) -> print code
+import TypeChecker.GrammarChecker ( checkProgram )
 
 typeCheckProgram p =
     runExceptT (runReaderT (checkProgram p) empty) 
